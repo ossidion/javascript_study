77bf8ce73261647ff79b4e462a4a5c4e286820af
Alexander Graham <alexinbristol@outlook.com>
2024-11-21 14:00:56 +0000

    Modulo, Interpolation and Indexing (JS)

---
+++ python.py
@@ -0,0 +1,5 @@
+team = 'red'
+player1Score = 25
+player2Score = 31
+
+print(f"The {team} team scored {player1Score + player2Score} points")
--- tutorial.js
+++ tutorial.js
@@ -1,1 +1,26 @@
-console.log("five" * "twenty")
+// console.log("five" * "twenty")
+console.log(5*20)
+
+
+// Declaring and initializing a variable:
+const greeting = "Hello there!"
+
+
+// modulo for expressing odd and even numbers. 
+ let odd = 15 % 2 // = 0.5 (1)
+ let even = 14 % 2 // = 0
+
+
+// Interpolation (f strings in Python). 
+const team = 'red'
+const player1Score = 25
+const player2Score = 31
+
+const description = `The ${team} team scored ${player1Score + player2Score} points` // Note the third type of quotation marks.
+// The red team scored 56 points
+
+
+// Accessing the last character in the string i.e. find length of string then minus one as string starts at 0. 
+const blogPost = '...and that\'s how I learned how to access the last character in a string!'
+const lastCharacter = blogPost[blogPost.length - 1] // '!'
+
