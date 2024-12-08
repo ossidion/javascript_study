89a89daebd92336310f4ce4df31dbb36bd00b2c6
Alexander Graham <alexinbristol@outlook.com>
2024-12-08 15:11:31 +0000

    Questions added

--- test.js
+++ test.js
@@ -191,6 +191,15 @@ transferProduct(shelf, basket, "apple");
 
 
 
+
+// This function takes an object containing a subject key and a grade value expressed as a number out of 100.
+
+// Iterate through the grades object, and for each subject:
+
+// If the grade number value is 70 or above, amend the value to "pass".
+// Otherwise, change the grade to "fail".
+// The function should return the updated grades object.
+
 // function updateStudentGrades(grades) {
 //     const gradesVariable = grades
 //     let passOrFail = ""
@@ -204,9 +213,6 @@ transferProduct(shelf, basket, "apple");
 // };
 
 
-
-
-
 // updateStudentGrades({ maths: 85 });
 // // should return { maths: "pass" }
 
@@ -218,3 +224,140 @@ transferProduct(shelf, basket, "apple");
 
 // updateStudentGrades({ maths: 85, english: 60 });
 // // should return { maths: "pass", english: "fail" }
+
+
+
+
+
+// This function takes a string of at least one character, and it should return the middle character(s) from that string.
+
+// If the string is of odd length, the function should return a single character. If the string is of even length, 
+// it should return the middle two characters concatenated together.
+
+function returnMiddleCharacter(string) {
+    // Write your code here
+    }
+
+    middleOfTheMiddle("dog");
+    // should return "o"
+    
+    middleOfTheMiddle("dogs");
+    // should return "og"
+    
+    middleOfTheMiddle("sweet candy")
+    // should return " "
+    
+    middleOfTheMiddle("I'll be back");
+    // should return "be"
+    
+    middleOfTheMiddle("The quick brown fox jumps over the lazy dog");
+    // should return "u"
+
+
+// This function takes a multi-dimensional array as an argument. It should return an array containing the first element of
+// the first nested array, and the last element of the last nested array. If the input array only contains one nested array,
+// the first and last element will come from the same array.
+
+// If the original input array is empty (i.e. no nested arrays within it), then the function should return an empty array.
+
+function getNestedFirstAndLast(array) {
+    // Write your code here
+    }
+
+getNestedFirstAndLast([[ 16 ], [ 82 ]]);
+// should return [ 16, 82 ]
+
+getNestedFirstAndLast([[ 2, 79, 13 ], [ 16, 34, 45 ], [ 21, 9, 98 ]]);
+// should return [ 2, 98 ]
+
+getNestedFirstAndLast([[ 16, 22 ]]);
+// should return [ 16, 22 ]
+
+getNestedFirstAndLast([]);
+// should return []
+
+
+
+
+// This function takes an array of objects representing graduates with three properties, name, course and graduationYear.
+
+// The function should add a new property to each object called jobTitle with a value based on the graduate's course and graduationYear:
+
+// If the course is Software Development and the graduationYear is before 2020, their jobTitle should be "Senior Software Developer".
+// If the course is Software Development and the graduationYear is 2020 or after, their jobTitle should be "Junior Software Developer".
+// If the course is Data Engineering and the graduationYear is before 2020, their jobTitle should be "Senior Data Engineer".
+// If the course is Data Engineering and the graduationYear is 2020 or after, their jobTitle should be "Junior Data Engineer".
+// The function should then return the array of graduates with the new property on each object.
+
+function addJobTitle(graduates) {
+    // Write your code here
+    }
+
+addJobTitle([{ name: "Jonny JavaScript", course: "Software Development", graduationYear: 2018 }]);
+// should return [
+//   { 
+//     name: "Jonny JavaScript",
+//     course: "Software Development",
+//     graduationYear: 2018,
+//     jobTitle: "Senior Software Developer"
+//   }
+// ]
+
+addJobTitle([{ name: "Petra Python", course: "Data Engineering", graduationYear: 2022 }]);
+// should return [
+ //  { 
+//     name: "Petra Python",
+//     course: "Data Engineering",
+//     graduationYear: 2022,
+//     jobTitle: "Junior Data Engineer"
+//   }
+// ]
+
+addJobTitle([
+  { name: "Jonny JavaScript", course: "Software Development", graduationYear: 2020 },
+  { name: "Petra Python", course: "Data Engineering", graduationYear: 2019 }
+  ]);
+// should return [
+//    { 
+//      name: "Jonny JavaScript", 
+//      course: "Software Development",
+//      graduationYear: 2020,
+//      jobTitle: "Junior Software Developer"
+//    },
+//    { 
+//      name: "Petra Python",
+//      course: "Data Engineering",
+//      graduationYear: 2019,
+//      jobTitle: "Senior Data Engineer"
+//    }
+// ]
+
+
+// This function takes a multi-dimensional array as an argument. Hidden somewhere within this array, there may be the letter X.
+
+// If there is no X in any nested array, the function should return a string of: "No X found"
+
+// Otherwise, the function should return a string stating the row and column of where the X was found: "X was found on row Y and 
+// column Z"
+
+function findTheX(grid) {
+    // Write your code here
+    }
+
+    findTheX([
+  ["S", "S", "S"],
+  ["S", "S", "S"] 
+]);
+// should return: "No X found"
+
+findTheX([
+  ["X", "S", "S"]
+]);
+// should return: "X was found on row 0 and column 0"
+
+findTheX([
+  ["S", "S", "S"],
+  ["S", "S", "S"],
+  ["S", "X", "S"] 
+]);
+// should return: "X was found on row 2 and column 1"
