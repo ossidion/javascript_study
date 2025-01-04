df1c3e5388b025adf4da9728e0a988c99714e1c3
Alex <alexinbristol@outlook.com>
2025-01-04 16:58:35 +0000

    Progressed pre-course

--- pre-course.js
+++ pre-course.js
@@ -24,11 +24,46 @@
 //     return console.log(myArray)
 //   }
 
-const myArray = [1, 2, 3, 4, 5]
-const min = Math.min(...myArray)
-const max = Math.max(...myArray)
-console.log(min)
+// const myArray = [1, 2, 3, 4, 5]
+// const min = Math.min(...myArray)
+// const max = Math.max(...myArray)
+// console.log(min)
 
 
 // findSmallestAndBiggest([1, 2, 3, 4, 5]) //returns [1, 5];
 
+
+
+
+
+
+// function isEvenLength(string) {
+//     let answer = false
+//     if (string.length % 2 == 0)
+//       answer = true
+//     return answer
+//   }
+
+
+
+
+
+function areStringsEqualCaseInsensitive(string1, string2) {
+    let answer = false
+    if (string1.lower === string2.lower) {
+      answer = true
+    }
+    return console.log(answer)
+  }
+
+
+
+areStringsEqualCaseInsensitive('hello', 'hello') // returns true
+
+areStringsEqualCaseInsensitive('hello', 'HELLO') // returns true
+
+areStringsEqualCaseInsensitive('banANA', 'BANana') // returns true
+
+areStringsEqualCaseInsensitive('hello', 'hella') // returns false
+
+areStringsEqualCaseInsensitive('apple', 'BANANA') // returns false
